cf3e1b8552dc965ac55e9b66affec4c538b23a2d
Leo <workspace2707@yandex.ru>
2024-03-12 22:04:05 +0300

    fixed slider, added images to slider

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -2,16 +2,19 @@ const prevBtn = document.getElementById("prevBtnHero");
 const nextBtn = document.getElementById("nextBtnHero");
 const slides = document.querySelector(".hero__slides");
 const totalSlides = document.querySelectorAll(".hero__slide").length;
-
+const totalSlidesArr = document.querySelectorAll(".hero__slide");
+console.log(totalSlidesArr);
 let slideIndex = 0;
 
 nextBtn.addEventListener("click", () => {
   slideIndex = (slideIndex + 1) % totalSlides;
+  if (slideIndex === totalSlides - 1) return;
   updateSlidePosition();
 });
 
 prevBtn.addEventListener("click", () => {
   slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+  if (slideIndex === totalSlides - 1) return;
   updateSlidePosition();
 });
 
